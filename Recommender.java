dad80da5c11630a6f647b4b5d3688f77eebe89b9
Nitesh <nkanand4@gmail.com>
2012-06-14 22:43:10 -0400

    Prevented comparing already compared files. Extracted base names out of file names.
    Added preventive measures to compare files already compared. Added
    method to extract basename of files, assuming '/' as separator of file
    path.

--- Recommender.java
+++ Recommender.java
@@ -8,7 +8,6 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Writer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -17,7 +16,7 @@ import java.util.Map.Entry;
 public class Recommender {
 	
 	private HashMap<String, ArrayList<HashMap<String, Integer>>> data = new HashMap<String, ArrayList<HashMap<String,Integer>>>();
-	private HashMap<String, ArrayList<HashMap<String, Integer>>> result = new HashMap<String, ArrayList<HashMap<String,Integer>>>();
+	private ArrayList<String> fileMaps = new ArrayList<String>();
 	private String outputFilePath="output/part-r-00000";
 	private String logFilePath="output/log.txt";
 	FileWriter logStream;
@@ -31,6 +30,12 @@ public class Recommender {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	private String filebasename(String f) {
+		String[] fileFrags = f.split("/");
+		String fileName = fileFrags[fileFrags.length - 1];		
+		return fileName;
 	}
 	
 	private void preprocess() {
@@ -72,15 +77,25 @@ public class Recommender {
 		}
 	}
 	
+	private boolean isAlreadyCompared(String f1, String f2) {
+		boolean alreadyCompared = fileMaps.contains(f1 + f2) || fileMaps.contains(f2 + f1);
+		if(!alreadyCompared) {
+			fileMaps.add(f1 + f2);
+		}
+		return alreadyCompared;
+	}
+	
 	private void process() {
 		for(Entry<String, ArrayList<HashMap<String, Integer>>> entOuter : data.entrySet()) {
 			String keyOuter = entOuter.getKey();
+			keyOuter = filebasename(keyOuter);
 			ArrayList<HashMap<String, Integer>> outerList = entOuter.getValue();
 			for(Entry<String, ArrayList<HashMap<String, Integer>>> entInner : data.entrySet()) {
-				int match = 0;
-				double matchPC;
+				int match = 0, matchPC;
 				 String keyInner = entInner.getKey();
-				 if(!keyOuter.equals(keyInner)) {
+				 keyInner = filebasename(keyInner);
+				 // also check if not already compared
+				 if(!keyOuter.equals(keyInner) && !isAlreadyCompared(keyOuter, keyInner)) {
 					 //log("Comparing " + keyOuter + " with " + keyInner);
 					 // do not compare same 2 files.
 					 ArrayList<HashMap<String, Integer>> innerList = entInner.getValue();
@@ -107,9 +122,8 @@ public class Recommender {
 						 }
 					 }
 					 matchPC = match*200/(outerList.size()+innerList.size());
-					 //keyOuter = keyOuter.replaceAll("file:/Users/nanand1/Downloads/DocumentMiner/input/", "");
-					 //keyInner = keyInner.replaceAll("file:/Users/nanand1/Downloads/DocumentMiner/input/", "");
-					 System.out.println(keyOuter + " matches " + keyInner + " by " + matchPC);
+					 if(matchPC > 30)
+						 System.out.println(keyOuter + " matches " + keyInner + " by " + matchPC);
 				 }
 				 
 			}
@@ -123,7 +137,6 @@ public class Recommender {
 		Recommender r = new Recommender();
 		r.preprocess();
 		r.process();
-
 	}
 
 }
